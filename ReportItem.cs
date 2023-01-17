c87de4eabc1d8ed9da4858c2f803b95a645f0872
mario macario <104312943+mario881@users.noreply.github.com>
2023-01-17 12:47:06 +0100

    Add project files.

---
+++ ReportItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace  WisejWebDesktopApplication1
+
+{
+    public class ReportItem
+	{
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public int Qty { get; set; }
+		public decimal Total => Price * Qty;
+	}
+}
---
+++ Window1.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.IO;
+using Wisej.Web;
+
+#if NET7_0
+using Microsoft.Reporting.NETCore;
+#endif
+
+#if NET48
+using Microsoft.Reporting.WinForms;
+#endif
+
+namespace WisejWebDesktopApplication1
+{
+    public partial class Window1 : Form
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var report = new LocalReport();
+
+            var items = new[] { new ReportItem { Description = "Widget 6000", Price = 104.99m, Qty = 1 }, new ReportItem { Description = "Gizmo MAX", Price = 1.41m, Qty = 25 } };
+            var parameters = new[] { new ReportParameter("Title", "Invoice 4/2020") };
+            using var fs = new FileStream("Report.rdlc", FileMode.Open);
+            report.LoadReportDefinition(fs);
+            report.DataSources.Add(new ReportDataSource("Items", items));
+            report.SetParameters(parameters);
+
+            byte[] buffer = report.Render("PDF", null);// x.Render("PDF",null);
+            Application.DownloadAndOpen("_blank", new System.IO.MemoryStream(buffer), "Stampa.pdf");
+        }
+    }
+}
